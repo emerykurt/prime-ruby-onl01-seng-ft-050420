1d708170f45d8889c905eec44df572c8c9d5362c
emerykurt <emerylumsden@gmail.com>
2020-05-10 23:01:14 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+require 'pry'
+def prime?(val)
+    if val <= 1                      
+        return false                       
+    else                            
+        (2..val - 1).to_a.all? do |i|
+            val % i != 0 
+    end
+end
+#true
+end  
